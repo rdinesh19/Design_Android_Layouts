80760848bf8f84742f210ec64645517f81541798
rdinesh19 <42532648+rdinesh19@users.noreply.github.com>
2020-07-25 00:48:01 +0530

    Initial commit

---
+++ app/src/main/java/com/dinesh/design_android_layouts/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dinesh.design_android_layouts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
